e1536079ef7eca31efc2df76d4cbc81b1adb65c9
3671069 <harian.elyoth@gmail.com>
2020-05-13 21:29:57 +0200

    start passe_1

--- src/pass1.c
+++ src/pass1.c
@@ -13,27 +13,25 @@
 #include "env.h"
 
 
-
 void passe_1(node_t root){
+    //Partons du principe que push global context met directement les variables
+    //globales dans le context global et initialise les offset
+    //On pourrait directement appeler pass_1 sur le root->opr[1] soit le NODE FUNC
+    
+    switch(root->nature)
+    {
+        case NODE_PROGRAM :
+            push_global_context();
+            passe_1(root->opr[1]);
+            break;
+        
+        case NODE_IDENT : //plusieurs cas 
 
-    push_global_context();
+    }
 
     // *Analyse des declarations des variables globales*
 
-    pop_context()
 
-    /*switch(root->nature){
-        case NODE_PROGRAM :
-            push_global_context();
-            break;
-        case NODE_IDENT :
-            break;
-        case NODE_TYPE :
-            break;
-        case NODE_DECL :
-            break;
-        default :
-            break;
-    
-    }*/
+
+    }
 }
--- src/testMinic.c
+++ src/testMinic.c
@@ -2,4 +2,6 @@ int a = 1, b;
 bool c = true;
 void main()
 {
+    c = false;
+    b = a + 12;
 }
